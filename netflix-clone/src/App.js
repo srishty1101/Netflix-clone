99fb0a3b387230362fc99cc195c6a61c11bf157a
Srishty Sharma <84490597+srishty1101@users.noreply.github.com>
2022-06-03 11:59:00 +0530

    Update App.js

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -9,7 +9,7 @@ import Banner  from './Banner';
 function App() {
   return (
     <div className="App">
-      
+      <Nav/>
      <Banner/>
       <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} isLargeRow />
       <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
